8072701c7c9ade6beab79c14130b2aa9b8f46657
timothyPortable <timothy.aknine@gmail.com>
2023-05-12 14:49:27 +0300

    f

--- src/main/java/com/example/demo/MyHttpServer.java
+++ src/main/java/com/example/demo/MyHttpServer.java
@@ -25,7 +25,7 @@ public class MyHttpServer {
 
     public static void main(String[] args) throws IOException {
 
-        int port = process.env.PORT
+        int port = process.env.PORT;
         //int port = Integer.parseInt(System.getenv("PORT"));
         //int port = 8080;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
